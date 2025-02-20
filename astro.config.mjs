225bca9593233c0862e58fe9320039ce1cbfb139
Marius Kimmina <mar.kimmina@gmail.com>
2025-02-20 19:43:48 +0100

    initial commit
    Signed-off-by: Marius Kimmina <mar.kimmina@gmail.com>

---
+++ astro.config.mjs
@@ -0,0 +1,39 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import remarkToc from "remark-toc";
+import remarkCollapse from "remark-collapse";
+import rehypeSlug from 'rehype-slug';
+import sitemap from "@astrojs/sitemap";
+import mdx from '@astrojs/mdx';
+
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://mariuskimmina.com",
+  // replace this with your deployed domain
+  integrations: [tailwind({
+      applyBaseStyles: false
+    }),
+    react(),
+    sitemap(),
+    mdx()
+  ],
+  markdown: {
+    remarkPlugins: [remarkToc, [remarkCollapse, {
+      test: "Table of contents"
+    }]],
+    rehypePlugins: [rehypeSlug],
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true
+    },
+    extendDefaultPlugins: true
+  },
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"]
+    }
+  },
+  scopedStyleStrategy: "where",
+});
